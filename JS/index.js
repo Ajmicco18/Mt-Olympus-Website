d704d4e8c88df40cc7ae7c997e897d4ddb634ebe
Anthony Micco <anthonymicco2003@gmail.com>
2024-11-20 23:53:40 +0300

    completed home page and added JS file and home page image

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+function submitNewsletter(e) {
+    e.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let number = document.getElementById("phone").value;
+
+    let optIn = ""
+
+    if (document.getElementById("yes").checked) {
+        optIn = document.getElementById("yes").value
+    }
+    else if (document.getElementById("no").checked) {
+        optIn = document.getElementById("no").value
+    }
+    else {
+        optIn = ""
+    }
+
+    if (email != "" && number != "" && optIn != "") {
+        alert("Form submitted successfully!")
+        document.getElementById("email").value = ""
+        document.getElementById("phone").value = ""
+    }
+    else {
+        alert("Error in submission! Please try again!")
+    }
+}
