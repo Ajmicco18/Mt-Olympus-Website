8dee2b9ff9e805aa5ace2b3fef1a41a677007176
Anthony Micco <anthonymicco2003@gmail.com>
2024-11-29 14:58:47 +0200

    started working on tourism page

--- JS/index.js
+++ JS/index.js
@@ -103,5 +103,44 @@ accordionHeaders.forEach(header => {
     });
 });
 
+/*****TOURISM PAGE JS*****/
+class Attraction {
+    constructor(id, name, descr, locationURL, imageURL) {
+        this.id = id;
+        this.name = name;
+        this.descr = descr;
+        this.locationURL = locationURL;
+        this.imageURL = imageURL;
+    }
+}
+
+const attractions = [
+    new Attraction(1, "Mount Olympus National Park", "A UNESCO Biosphere Reserve, this park offers stunning biodiversity,hiking trails,and scenic views. It's home to diverse flora and fauna, including endemic species.", "https://www.google.com/maps/dir//Mount+Olympus+National+Park", "../Images/olympus-park.png"),
+    new Attraction(2, "The Muses Plateau", "Named after the nine Muses of Greek mythology, this plateau offers breathtaking views and is an ideal spot for hiking. The landscape is dotted with alpine meadows and ancient stone paths.", "https://www.google.com/maps/dir//Unnamed+Road,+Litochoro+602+00/", "../Images/muses-plateau.png"),
+    new Attraction(3, "The Ancient City of Dion", "An archaeological site and ancient Greek city dedicated to Zeus, Dion was a major religious center. The ruins include temples, a theater, and sanctuaries. It also houses a museum displaying artifacts.", "https://www.google.com/maps/dir//Dion+601+00/@40.1738794,22.4090548,12z/", "../Images/dion-tour.png"),
+    new Attraction(4, "Litochoro", "A town at the foot of Mount Olympus known as the Gateway to Olympus, Litochoro is a charming town with a rich history. It's a base for hikers and offers access to the mountain's trails, plus local tavernas and shops.", "https://www.google.com/maps/dir//Litochoro,+602+00/@40.1030494,22.4202095,12z/", "../Images/litochoro.png"),
+    new Attraction(5, "Olympus Ski Resort", " A popular winter destination offering skiing, snowboarding, and snowshoeing in the winter months, along with hiking in summer. It provides spectacular views of the mountain and surrounding area.", "https://www.google.com/maps/dir//Elassona+402+00/@40.0421366,22.259119,12z/", "../Images/olympus-ski.png"),
+    new Attraction(6, "The Monastery of St. Dionysios", "This monastery is dedicated to Saint Dionysios, the patron saint of Mount Olympus. It is perched on a cliff and offers peaceful surroundings, with impressive Byzantine architecture and historical significance.", "https://www.google.com/maps/dir//Litochoro+602+00/@40.1182808,22.4022891,12z/", "../Images/st-dionysios.png")
+]
+
+attractions.forEach((attraction) => {
+    addAttraction(attraction);
+})
+
+function addAttraction(attraction) {
+    let html = `
+    <div class = "attraction" id="attraction-${attraction.id}" style="background-image: url(${attraction.imageURL})">
+        <div id="text">
+            <h3>${attraction.name}</h3>
+            <p>${attraction.descr}</p>
+            <a href="${attraction.locationURL}">Directions</a> 
+        </div>
+    </div>`
+
+    //insertAdjacentHTML gets two arguments: First is the position, second is HTML
+    //The four possible positions are: "beforeBegin", "afterBegin", "beforeEnd", "afterEnd"
+    document.getElementById("attraction-container").insertAdjacentHTML("beforeend", html);
+}
+
 
 
